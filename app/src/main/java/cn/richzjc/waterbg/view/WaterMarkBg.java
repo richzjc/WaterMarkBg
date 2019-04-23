a1a0c27293d8486f8c434022c9ac42f30c2b0b48
richzjc <zhangjianchuan@wallstreetcn.com>
2019-04-23 10:00:37 +0800

    支持了水印的效果

--- app/src/main/java/cn/richzjc/waterbg/view/WaterMarkBg.java
+++ app/src/main/java/cn/richzjc/waterbg/view/WaterMarkBg.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.BitmapDrawable;
@@ -35,21 +36,39 @@ public class WaterMarkBg extends Drawable {
         canvas.drawColor(Color.TRANSPARENT);
         canvas.save();
         Drawable drawable = context.getResources().getDrawable(drawableId);
-        BitmapDrawable bd = (BitmapDrawable)drawable;
-        if(bd == null) return;
+        BitmapDrawable bd = (BitmapDrawable) drawable;
+        if (bd == null) return;
         Bitmap bitmap = bd.getBitmap();
-        if(bitmap == null) return;
+        if (bitmap == null) return;
+        bitmap = rotate(bitmap, degress);
         int w = drawable.getIntrinsicWidth();
         int h = drawable.getIntrinsicHeight();
-        canvas.rotate(degress);
         int index = 0;
-        for (int positionY = -(h / 3); positionY <= height; positionY += h+80) {
+        for (int positionY = -(h / 3); positionY <= height; positionY += h + 80) {
             float fromX = -w + (index++ % 2) * w;
             for (float positionX = fromX; positionX < width; positionX += w * 1.5) {
-                    canvas.drawBitmap(bitmap, positionX, positionY, paint);
+                canvas.drawBitmap(bitmap, positionX, positionY, paint);
             }
         }
-        canvas.restore();
+    }
+
+    private static Bitmap rotate(Bitmap b, int degrees) {
+        if (degrees == 0) {
+            return b;
+        }
+        if (degrees != 0 && b != null) {
+            Matrix m = new Matrix();
+            m.setRotate(degrees, (float) b.getWidth(), (float) b.getHeight());
+            try {
+                Bitmap b2 = Bitmap.createBitmap(b, 0, 0, b.getWidth(), b.getHeight(), m, true);
+                if (b != b2) {
+                    b.recycle();
+                    b = b2;
+                }
+            } catch (OutOfMemoryError ex) {
+            }
+        }
+        return b;
     }
 
     @Override
